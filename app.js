e834e8c1d0ed9b8571ad4252e617ed6ed3fe2939
Frank Salazar Jr <franksalazarjr.@Franks-MacBook-Pro.local>
2021-06-16 12:57:16 -0500

    Added h2

--- app.js
+++ app.js
@@ -252,6 +252,8 @@ if(ingredients.includes('flour')) {
 }
 
 let animals = ['shark', 'salmon', 'whale', 'bear', 'lizard', 'tortoise'];
-let swimmers = animals.slice(0, 3);
-let mammals = animals.slice(2, 4);
-let reptiles = animals.slice(4, 6);
+// let swimmers = animals.slice(0, 3);
+// let mammals = animals.slice(2, 4);
+// let reptiles = animals.slice(4, 6);
+animals.splice(1, 0, 'octopus');
+
