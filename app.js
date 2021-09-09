99061f5a5e21e0725f2053b4a4a3dea0200c92be
u241c <jr4701.fsj@gmail.com>
2021-09-08 19:58:31 -0500

    WIP

--- app.js
+++ app.js
@@ -542,6 +542,31 @@ console.log(avg);
 
 
 
+//Functions
+// function rollDie(){
+//     let roll = Math.floor(Math.random() * 6) + 1;
+//     console.log(`Rolled: ${roll}`);
+// }
+
+// function throwDice(){
+//     rollDie();
+//     rollDie();
+//     rollDie();
+//     rollDie();
+//     rollDie();
+//     rollDie();
+//
+
+function square(num){
+    console.log(num * num);
+}
+
+function sum(x, y){
+    console.log(x+y);
+}
+
+
+
 
 
 
