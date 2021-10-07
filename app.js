5276c2e6ed417a4a44e3193e8833b7d5546f092d
u241c <jr4701.fsj@gmail.com>
2021-10-07 06:41:27 -0500

    finished 119

--- app.js
+++ app.js
@@ -940,3 +940,23 @@ let day = 17;
 // function fullName(first, last,){
 //   console.log(arguments)
 // }
+
+//119. shorthand object properties
+
+// const getStats = (arr) =>{
+//   const max = Math.max(...arr);
+//   const min = Math.min(...arr);
+//   const sum = arr.reduce((sum, r) => sum + r);
+//   const avg = sum / arr.length;
+
+// return {
+//   max: max,
+//   min: min,
+//   sum: sum,
+//   avg: avg,
+// }
+// }
+// const reviews = [4.5, 5.0, 3.44, 2.8, 3.5, 4.0, 3.5];
+
+// const stats = getStats(reviews);
+
