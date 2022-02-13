4551b89ac2c8443c621d44cb1f6a91f6747768f6
u241c <jr4701.fsj@gmail.com>
2022-02-13 13:59:22 -0600

    if statements

--- app2.js
+++ app2.js
@@ -1,2 +1,14 @@
-console.log("i am not a robot");
+let behavioralScore = 1;
+
+if (behavioralScore === 3){
+    console.log('Leader in the house!');
+}
+else if (behavioralScore === 2){
+    console.log('improvement required!');
+}
+else {
+    console.log('Say bye bye!');
+}
+
+
 
