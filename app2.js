67065ba6ee338ef9689ca1fe3a3b1ecb285224ae
u241c <jr4701.fsj@gmail.com>
2022-02-14 13:42:32 -0600

    working on nesting conditionals

--- app2.js
+++ app2.js
@@ -1,14 +1,25 @@
-let behavioralScore = 1;
+// let behavioralScore = 1;
 
-if (behavioralScore === 3){
-    console.log('Leader in the house!');
-}
-else if (behavioralScore === 2){
-    console.log('improvement required!');
-}
-else {
-    console.log('Say bye bye!');
-}
+// if (behavioralScore === 3){
+//     console.log('Leader in the house!');
+// }
+// else if (behavioralScore === 2){
+//     console.log('improvement required!');
+// }
+// else {
+//     console.log('Say bye bye!');
+// }
 
 
+// let password = 'Hello hgjgjk';
 
+// if (password.length >= 6){
+//     if (password.indexOf (' ') === -1 );{
+//     console.log("Valid Password");
+// }
+// else if(password.indexOf(' ') !== -1)
+//     console.log('No spaces allowed.')
+// }
+// else {
+//     console.log('Invalid Password.')
+// }
