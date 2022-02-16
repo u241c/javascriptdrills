e62958438ac6e17a72d238fe4dd8da0745c3e296
u241c <jr4701.fsj@gmail.com>
2022-02-16 12:51:09 -0600

    Switch practice

--- app2.js
+++ app2.js
@@ -20,16 +20,65 @@
     //console.log('falsy');
 //}       Value 10 is truthy, however if placed a value of NaN, 0, empty string, false, undefined will come out falsy.
 
-let loggin = 'Hellokitty';
+// let loggin = 'Hellokitty';
 
-if (loggin.length >= 6){
-    if(loggin.indexOf (' ') !== -1) {
-        console.log('No Spaces');
-}
-else{
-    console.log('Valid');
-}
-}
-else{
-    console.log('Invalid Password');
+// if (loggin.length >= 6){
+//     if(loggin.indexOf (' ') !== -1) {
+//         console.log('No Spaces');
+// }
+// else{
+//     console.log('Valid');
+// }
+// }
+// else{
+//     console.log('Invalid Password');
+// }
+// switch 
+
+// let days = 0;
+
+// switch(days){
+//     case 0:
+//       console.log("Monday");
+//       break;
+//     case 1:
+//       console.log("Tuesday");
+//       break;
+//     case 2:
+//       console.log("Wednesday");
+//       break;
+//     case 3:
+//       console.log("Thursday");
+//       break;
+//     case 4:
+//       console.log("Friday");
+//       break;
+//     case 5:
+//       console.log("Saturday");
+//       break;
+//     case 6:
+//       console.log("Sunday");
+//       break;
+//     default:
+//         console.log('Whaaaat')  
+// }
+
+
+let crue = 0;
+
+switch(crue){
+    case 0:
+    console.log('Nikki');
+    break;
+    case 1:
+    console.log('Tommy');
+    break;
+    case 2:
+    console.log('Vince');
+    break;
+    case 3:
+    console.log('Mick');
+    break;
+    default: 
+    console.log('Whoooo!');
 }
