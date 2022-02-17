de8f5f0cb8d4adb14084cc3b0150cdbb97beeabf
u241c <jr4701.fsj@gmail.com>
2022-02-17 16:50:02 -0600

    working with arrays

--- app3.js
+++ app3.js
@@ -2,8 +2,13 @@
 // let roomList = ['guitar', 'wicker chair', 'computer', 'desk', 'printer'];
 
 //learning to modifie arrays 
-
-
 // let newArray = [1, 3, 20.00, 'sign'];
-
 // learning conta
+// let fruits = ['kiwi', 'strawberrys'];
+// let veg = ['lettus', 'tomato'];
+// let bread = ['sour doagh', 'french'];  // let allFoods = (fruits.concat(veg, bread));
+
+let crue = ['live wire', 'dr. feelgood'];
+let ratt = ['Dance', 'lay it down'];
+let extreme = ['more than', 'monster'];
+let allSongs= (crue.concat(ratt, extreme));
