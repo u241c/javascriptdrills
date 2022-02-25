693af40bceb19c781de566e23f524891c3303ae2
u241c <jr4701.fsj@gmail.com>
2022-02-24 19:06:21 -0600

    Wip

--- app3.js
+++ app3.js
@@ -20,4 +20,6 @@
 // let animal = ['bear','dog', 'cat'];
 // animal.slice(0, 2)  'bear', 'dog' will appear
 
-// working with const in an Array. the reference in const is unable to change. but you can change what is inside the array in const. NOTE the reference never changes. So using push, pop, shift, and notshift are good to use. 
+// working with const in an Array. the reference in const is unable to change. but you can change what is inside the array in const. NOTE the reference never changes. So using push, pop, shift, and unshift are good to use. 
+
+
