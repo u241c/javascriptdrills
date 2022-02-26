a3adca38982889a4fcc61c75ea2a16dc132f163b
u241c <jr4701.fsj@gmail.com>
2022-02-26 13:24:03 -0600

    accessing values withing the object

--- app3.js
+++ app3.js
@@ -12,22 +12,31 @@
 // let ratt = ['Dance', 'lay it down'];
 // let extreme = ['more than', 'monster'];
 // let allSongs= (crue.concat(ratt, extreme));
-
-
 // let people = ['e', 'd','a', 'r', 't'];
 
 // when using the slice method is quite like using a trimming example
 // let animal = ['bear','dog', 'cat'];
 // animal.slice(0, 2)  'bear', 'dog' will appear
-
 // working with const in an Array. the reference in const is unable to change. but you can change what is inside the array in const. NOTE the reference never changes. So using push, pop, shift, and unshift are good to use. 
-
 // creating objects
 //example: const foodBar = {
 //     key(name): value,
 // };
-const userReviews = {}
-userReviews['queenbee'] = 4.5;
-userReviews.drFeelgood = 2.0;
-userReviews.drFeelgood += 2;
-userReviews['queenbee'] -= 2;
+// const userReviews = {}
+// userReviews['queenbee'] = 4.5;
+// userReviews.drFeelgood = 2.0;
+// userReviews.drFeelgood += 2;
+// userReviews['queenbee'] -= 2;
+
+// const student = {
+//     firstName: 'bob',
+//     lastName: 'the builder',
+//     strength: ['builden', 'music'],
+//     exams: {
+//         midterm: 92,
+//         final: 88
+//     }
+// }; 
+// const completeAvg = (student.exam.midterm + student.exam.final) % 2;
+//  or how to access value would look like:
+//  student.strength[1] would be "music"
